998ff6a2b80af5f9958f722ccfcd6ee8437587b1
Margaux Sleckman <msleckman@usgs.gov>
2021-11-15 17:01:33 -0800

    updates to the repo, split script into fetch and process script, added visualize folder

---
+++ 1_fetch/src/data_extraction_ms.R
@@ -0,0 +1,59 @@
+################################
+# Model_RMSE data extraction   #
+#                              #
+# Author:                      #     
+# Date:                        #
+################################
+
+
+## Libs
+library(dplyr)
+library(readr)
+library(sbtools)
+
+
+## Check that you are in project home directory - should be in project home directory
+print(getwd())
+
+## Global Variables
+
+out_path <- '1_fetch/out'
+csv_filename_output <- 'model_RMSEs.csv'
+sb_id_sciencebase <- '5d925066e4b0c4f70d0d0599'
+
+
+### FUNCTION 1 - download the right data from Sciencebase. This function returns the path to the downloaded data 
+
+data_extraction <- function(filename_output_csv,
+                            download_data_location,
+                            selected_id_sciencebase,
+                            sb_filenames = 'me_RMSE.csv') {
+  # print()
+  ## Set output path + path
+  downloaded_data_path <- file.path(download_data_location,
+                                    filename_output_csv)
+  
+  ## Get the file from Science Base
+  sbtools::item_file_download(sb_id = selected_id_sciencebase,
+                              names = sb_filenames,
+                              destinations = downloaded_data_path,
+                              overwrite_file = TRUE)
+  print(downloaded_data_path)
+  ## Check if it worked - file download and location check check
+  if (file.exists(downloaded_data_path) == T) {
+    print(paste('Downloaded file exists in', downloaded_data_path))
+  } else{
+    print(paste('The file does not exist in the designated folder:', downloaded_data_path))
+  }
+  
+  # print(downloaded_data_path)
+  return (downloaded_data_path) 
+  
+}
+
+### RUN ###
+
+downloaded_data_path <- data_extraction(filename_output_csv = csv_filename_output,
+                                        download_data_location = out_path,
+                                        selected_id_sciencebase = sb_id_sciencebase)
+
